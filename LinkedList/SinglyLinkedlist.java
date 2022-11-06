637bc759699998599e83c8eef181f8f31d39521e
Vivek_Gupta <vivekgupta86988@gmail.com>
2022-11-06 09:05:45 +0530

    linkedlist

---
+++ LinkedList/SinglyLinkedlist.java
@@ -0,0 +1,95 @@
+package LinkedList;
+
+import java.util.LinkedList;
+
+import javafx.scene.Node;
+
+public class SinglyLinkedlist {
+    //head of list
+    static Node head;
+
+    static class Node{
+        int data;
+        Node next;
+
+        // constructor
+        Node(int d){
+            data=d;
+            next=null;
+        }
+
+    }
+
+
+    // insert the new_node at first node
+    public static void addFirst(SinglyLinkedlist list,int data){
+        Node new_node=new Node(data);
+        if(list.head==null){
+            list.head=new_node;
+        }
+        new_node.next=list.head;
+        list.head=new_node;
+        // if(head==null){
+        //     head=newNode;
+            
+        // }
+        // newNode.next=head;
+        // head=newNode;
+    }
+
+    // insert the new_node at last node
+   public static void insert(SinglyLinkedlist list,int data){
+    // new node
+    Node new_node=new Node(data);
+
+    // check empty and if yes then make new node as a head
+    if(list.head==null){
+        list.head=new_node;
+    }else{
+        Node last=list.head;
+        while(last.next!=null){
+            last=last.next;
+        }
+
+        // insert the new_node at last node
+        last.next=new_node;
+    }
+    // return list;
+    
+   }
+
+//Method to print the linkedlist
+public static void printList(SinglyLinkedlist list){
+    Node currNode=list.head;
+    System.out.print("LinkedList:");
+    // Traverse through the Linkedlist
+    while(currNode!=null){
+        // print the data at current node
+        System.out.print(currNode.data+" ");
+        // go to the next code
+        currNode=currNode.next;
+    }
+    System.out.println();
+}   
+
+   public static void main(String[] args) {
+    // empty linkedlist
+    SinglyLinkedlist list=new SinglyLinkedlist();
+
+    // insertion at last node
+    for(int i=0;i<11;i++){
+        insert(list, i);
+    }
+    // print the linkedlist
+    printList(list);
+    
+    // insertion at first node
+     addFirst(list,5);
+     addFirst(list,6);
+     addFirst(list,7);
+    
+    // print the linkedlist
+    printList(list);
+    
+   }    
+}
