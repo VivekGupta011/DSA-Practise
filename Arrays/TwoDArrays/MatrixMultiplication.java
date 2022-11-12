977800d86180f55148461af9ec39f3b8206ea339
Vivek_Gupta <vivekgupta86988@gmail.com>
2022-11-12 21:50:57 +0530

    2d array

---
+++ Arrays/TwoDArrays/MatrixMultiplication.java
@@ -0,0 +1,20 @@
+package Arrays.TwoDArrays;
+
+
+public class MatrixMultiplication {
+
+    public static void matrixMultiplication(int[][] TwoDArray1,int[][] TwoDArray2){
+    
+    }
+    public static void main(String[] args) {
+        for(int i=0;i<2;i++){
+            for(int j=0;j<2;j++){
+                for(int k=0;k<2;k++){
+                    System.out.println(i);
+                }
+            }
+        }
+        
+    }
+    
+}
