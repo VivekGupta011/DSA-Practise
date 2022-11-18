bb7487eb3a2b6a4c50ce821945b9df0f5ce6994e
Vivek_Gupta <vivekgupta86988@gmail.com>
2022-11-18 09:52:06 +0530

    added

---
+++ Recursion/FindArraySortedOrNotUsingRecursion.java
@@ -0,0 +1,20 @@
+package Recursion;
+
+public class FindArraySortedOrNotUsingRecursion {
+
+    public static boolean CheckSortOrNot(int[] array,int index){
+        
+        // Base Condition
+        if(index==array.length-1){
+            return true;
+        }
+        System.out.println("index:"+index);
+
+        return array[index]<array[index+1] && CheckSortOrNot(array, index+1);
+    }
+    public static void main(String[] args) {
+        int[] array={1,3,5,6,7,8,15,20};
+        System.out.println("Ans is:"+CheckSortOrNot(array,0));
+    }
+    
+}
