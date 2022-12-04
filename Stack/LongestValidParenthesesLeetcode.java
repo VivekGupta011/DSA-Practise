bc2c26c17fb04ff25e126a5d5731ca6558c4ee33
Vivek gupta <vivekgupta86988@gmail.com>
2022-12-04 19:42:40 +0800

    stack problem

---
+++ Stack/LongestValidParenthesesLeetcode.java
@@ -0,0 +1,34 @@
+package Stack;
+
+import java.util.Stack;
+
+public class LongestValidParenthesesLeetcode {
+    public static int longestValidParentheses(String str){
+        // creating a stack
+        Stack<Character> stack=new Stack<>();
+        int i=0;
+
+       if(str.isEmpty()){
+        return 0;
+       }else{
+        for(char c:str.toCharArray()){
+            if(c=='(' || c=='{' || c=='['){
+                stack.push(c);
+            }else if((c==')' && stack.peek()=='(') || (c=='}' && stack.peek()=='{') || (c==']' && stack.peek()=='[')){
+               stack.pop();
+               i=i+2;
+            }else{
+                i=i+0;
+            }
+        }
+       }
+
+        return i;
+    }
+    public static void main(String[] args) {
+        LongestValidParenthesesLeetcode obj=new LongestValidParenthesesLeetcode();
+        System.out.println("Ans is:" + obj.longestValidParentheses("(()"));
+        
+    }
+    
+}
--- Stack/ParenthesisChecker.java
+++ Stack/ParenthesisChecker.java
@@ -9,30 +9,31 @@ public class ParenthesisChecker {
     public static boolean isValid(String s) {
 
         // creating Character Stack
-        Stack<Character> stack=new Stack<>();
+        Stack<Character> stack = new Stack<>();
         // traversing String character
-        for(char c:s.toCharArray()){
-            if(c=='(' || c=='[' || c=='{'){
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
                 stack.push(c);
 
-            }else if(stack.isEmpty()){
+            } else if (stack.isEmpty()) {
                 return false;
-            }else if((c=='}' && stack.peek()=='{') || (c==')' && stack.peek()=='(') || (c==']' && stack.peek()=='[')){
+            } else if ((c == '}' && stack.peek() == '{') || (c == ')' && stack.peek() == '(')
+                    || (c == ']' && stack.peek() == '[')) {
                 stack.pop();
-            }else{
+            } else {
                 return false;
             }
 
         }
 
         return stack.isEmpty();
-        
-    }
-    public static void main(String[] args) {
-        ParenthesisChecker obj=new ParenthesisChecker();
-        System.out.println("Ans is:"+obj.isValid("([)]"));
-        
 
     }
-    
+
+    public static void main(String[] args) {
+        ParenthesisChecker obj = new ParenthesisChecker();
+        System.out.println("Ans is:" + obj.isValid("([)]"));
+
+    }
+
 }
