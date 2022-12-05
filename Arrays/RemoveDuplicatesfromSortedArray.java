408b6b8fc06d0208bddb439e6f43b0ed23e7f35b
Vivek gupta <vivekgupta86988@gmail.com>
2022-12-06 02:49:41 +0800

    array problem

--- Arrays/RemoveDuplicatesfromSortedArray.java
+++ Arrays/RemoveDuplicatesfromSortedArray.java
@@ -2,18 +2,22 @@ package Arrays;
 
 public class RemoveDuplicatesfromSortedArray {
     public static int removeDuplicates(int[] nums) {
-        int[] array2=new int[nums.length];
-        int count=1;
+       
+        int k=1;
+        int j=1;
         for(int i=0;i<nums.length-1;i++){
             if(nums[i]!=nums[i+1]){
-                 array2[count]=count;
-                 count++;
+                 nums[j]=nums[i+1];
+                 j++;
             }
         }
-        return count;
+        for(int c:nums){
+            System.out.print(c+" ");
+        }
+        return j;
     }
     public static void main(String[] args) {
-        int[] array={0,0,1,1,1,2,2,3,3,4};
+        int[] array={0,0,1,1,1,2,2,2,3,3,3,4};
         System.out.println("Ans is:"+removeDuplicates(array));
     }
     
