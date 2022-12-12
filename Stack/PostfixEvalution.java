812f3af1647d24b0700e3f769382217468ff9c80
Vivek gupta <vivekgupta86988@gmail.com>
2022-12-12 19:45:41 +0800

    prefix and postfix

--- Stack/PostfixEvalution.java
+++ Stack/PostfixEvalution.java
@@ -3,52 +3,52 @@ package Stack;
 import java.util.Stack;
 
 public class PostfixEvalution {
-    static double evaluatePrefix(String s) {
-        Stack < Double > Stack = new Stack < Double > ();
-    
-        for (int j =0;j<s.length()-1;j++) {
-    
-          // Push operand to Stack
-          // To convert exprsn[j] to digit subtract
-          // '0' from exprsn[j].
-          if (s.charAt(j)>='0' && s.charAt(j)<='9')
-            Stack.push((double) (s.charAt(j) -'0'));
-    
-          else {
-    
-            // Operator encountered
-            // Pop two elements from Stack
-            double o1 = Stack.peek();
-            Stack.pop();
-            double o2 = Stack.peek();
-            Stack.pop();
-    
-            // Use switch case to operate on o1
-            // and o2 and perform o1 O o2.
-            switch (s.charAt(j)) {
-            case '+':
-              Stack.push(o1 + o2);
-              break;
-            case '-':
-              Stack.push(o1 - o2);
-              break;
-            case '*':
-              Stack.push(o1 * o2);
-              break;
-            case '/':
-              Stack.push(o1 / o2);
-              break;
-            }
-          }
+  // Method to evaluate value of a postfix expression
+  static int evaluatePostfix(String exp) {
+    // create a stack
+    Stack<Integer> stack = new Stack<>();
+
+    // Scan all characters one by one
+    for (int i = 0; i < exp.length(); i++) {
+      char c = exp.charAt(i);
+
+      // If the scanned character is an operand (number here),
+      // push it to the stack.
+      if (Character.isDigit(c))
+        stack.push(c - '0');
+
+      // If the scanned character is an operator, pop two
+      // elements from stack apply the operator
+      else {
+        int val1 = stack.pop();
+        int val2 = stack.pop();
+
+        switch (c) {
+          case '+':
+            stack.push(val2 + val1);
+            break;
+
+          case '-':
+            stack.push(val2 - val1);
+            break;
+
+          case '/':
+            stack.push(val2 / val1);
+            break;
+
+          case '*':
+            stack.push(val2 * val1);
+            break;
         }
-    
-        return Stack.peek();
       }
-
-    public static void main(String[] args) {
-        String str = "823*+7/1-";
-        System.out.println("Ans is:" + evaluatePrefix(str));
-
     }
+    return stack.pop();
+  }
+
+  // Driver program to test above functions
+  public static void main(String[] args) {
+    String exp = "823*+7/1-";
+    System.out.println("postfix evaluation: " + evaluatePostfix(exp));
+  }
 
 }
--- Stack/PrefixEvalution.java
+++ Stack/PrefixEvalution.java
@@ -47,52 +47,109 @@ public class PrefixEvalution {
 
 
     // for Double stack
-      static double evaluatePrefix(String s) {
-        Stack < Double > Stack = new Stack < Double > ();
+    //   static double evaluatePrefix(String s) {
+    //     Stack < Double > Stack = new Stack < Double > ();
     
-        for (int j = s.length() - 1; j >= 0; j--) {
+    //     for (int j = s.length() - 1; j >= 0; j--) {
     
-          // Push operand to Stack
-          // To convert exprsn[j] to digit subtract
-          // '0' from exprsn[j].
-          if (s.charAt(j)>='0' && s.charAt(j)<='9')
-            Stack.push((double) (s.charAt(j) -'0'));
+    //       // Push operand to Stack
+    //       // To convert exprsn[j] to digit subtract
+    //       // '0' from exprsn[j].
+    //       if (s.charAt(j)>='0' && s.charAt(j)<='9')
+    //         Stack.push((double) (s.charAt(j) -'0'));
     
-          else {
+    //       else {
     
-            // Operator encountered
-            // Pop two elements from Stack
-            double o1 = Stack.peek();
-            Stack.pop();
-            double o2 = Stack.peek();
-            Stack.pop();
+    //         // Operator encountered
+    //         // Pop two elements from Stack
+    //         double o1 = Stack.peek();
+    //         Stack.pop();
+    //         double o2 = Stack.peek();
+    //         Stack.pop();
     
-            // Use switch case to operate on o1
-            // and o2 and perform o1 O o2.
-            switch (s.charAt(j)) {
-            case '+':
-              Stack.push(o1 + o2);
-              break;
-            case '-':
-              Stack.push(o1 - o2);
-              break;
-            case '*':
-              Stack.push(o1 * o2);
-              break;
-            case '/':
-              Stack.push(o1 / o2);
-              break;
-            }
+    //         // Use switch case to operate on o1
+    //         // and o2 and perform o1 O o2.
+    //         switch (s.charAt(j)) {
+    //         case '+':
+    //           Stack.push(o1 + o2);
+    //           break;
+    //         case '-':
+    //           Stack.push(o1 - o2);
+    //           break;
+    //         case '*':
+    //           Stack.push(o1 * o2);
+    //           break;
+    //         case '/':
+    //           Stack.push(o1 / o2);
+    //           break;
+    //         }
+    //       }
+    //     }
+    
+    //     return Stack.peek();
+    //   }
+
+    // public static void main(String[] args) {
+    //     String str = "-+9*45+20";
+    //     System.out.println("Ans is:" + evaluatePrefix(str));
+
+    // }
+
+    // gfg code
+    // Method to evaluate value of a postfix expression
+    static Boolean chk(char c) {
+      // If the character is a digit
+      // then it must be an operand
+      if (c >= 48 && c <= 57)
+        return true;
+      else
+        return false;
+    }
+  
+    static double evaluatePrefix(String s) {
+      Stack < Double > Stack = new Stack < Double > ();
+  
+      for (int j = s.length() - 1; j >= 0; j--) {
+  
+        // Push operand to Stack
+        // To convert exprsn[j] to digit subtract
+        // '0' from exprsn[j].
+        if (chk(s.charAt(j))){
+            Stack.push((double)(s.charAt(j) - 48));
+          
+        }
+        else {
+          // Operator encountered
+          // Pop two elements from Stack
+          double o1 = Stack.peek();
+          Stack.pop();
+          double o2 = Stack.peek();
+          Stack.pop();
+  
+          // Use switch case to operate on o1
+          // and o2 and perform o1 O o2.
+          switch (s.charAt(j)) {
+          case '+':
+            Stack.push(o1 + o2);
+            break;
+          case '-':
+            Stack.push(o1 - o2);
+            break;
+          case '*':
+            Stack.push(o1 * o2);
+            break;
+          case '/':
+            Stack.push(o1 / o2);
+            break;
           }
         }
-    
-        return Stack.peek();
       }
-
-    public static void main(String[] args) {
-        String str = "-+9*45+20";
-        System.out.println("Ans is:" + evaluatePrefix(str));
-
+  
+      return Stack.peek();
     }
-
+  
+    public static void main(String[] args) {
+      String s = "+5*46";
+      System.out.println(evaluatePrefix(s));
+    }
 }
