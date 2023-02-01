fd3d55c04e117cce848cf4a58813ecde091b00da
Vivek Gupta <vivekgupta86988@gmail.com>
2023-02-01 19:31:14 +0530

    probel,

--- OptimizeTechniquesTwoPointerApproach/IntersectionOfTwoArray.java
+++ OptimizeTechniquesTwoPointerApproach/IntersectionOfTwoArray.java
@@ -1,6 +1,8 @@
 package OptimizeTechniquesTwoPointerApproach;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class IntersectionOfTwoArray {
 
@@ -28,11 +30,54 @@ public class IntersectionOfTwoArray {
         return ans;
     }
 
+    public static List<Integer> targetIndices(int[] nums, int target) {
+
+        List<Integer> list = new ArrayList();
+
+        int count = 0;
+        int pos = 0;
+        // declare variables for target number count
+        // and 'sorted' position of target number
+
+        for (int i : nums) {
+            if (i == target)
+                count++;
+            // if number in nums is target number, add to count
+            if (i < target)
+                pos++;
+            // if the number is smaller than target, if sorted, would
+            // be placed before target number so by counting these
+            // we will know where the target number(s) would start appearing
+        }
+
+        for (int i = 0; i < count; i++) {
+            list.add(pos + i);
+            // for loop to calculate the indexes, the pos is incremented for
+            // each instance of the target number to create the answer
+        }
+
+        return list;
+
+    }
+
+   
+
     public static void main(String args[]) {
         int[] nums1 = { 1, 2, 2, 1 };
         int[] nums2 = { 2, 2 };
+        int[] nums3 = { 1, 2, 5, 2, 3 };
         int[] ans = intersect(nums1, nums2);
         for (int x : ans)
             System.out.print(x + " ");
+
+        System.out.println("Ans is:" + targetIndices(nums3, 2));
+
+        int[] arr = new int[nums3.length];
+        for (int i = 0; i < nums3.length; i++) {
+            arr[i] = nums3[i];
+        }
+
+        // arr=nums3;
+        System.out.println("clone array:" + Arrays.toString(arr));
     }
 }
--- Stack/Monotonic_Stack/Monotonic_Increasing_Stack.java
+++ Stack/Monotonic_Stack/Monotonic_Increasing_Stack.java
@@ -1,6 +1,8 @@
 package Stack.Monotonic_Stack;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Stack;
 
 public class Monotonic_Increasing_Stack {
@@ -61,6 +63,52 @@ public class Monotonic_Increasing_Stack {
 
         // Time Complexity: O(N)
         // Auxiliary Space: O(N)
+        StringBuilder sb = new StringBuilder();
+        sb.append("v ");
+        sb.append("V1 ");
+        sb.append("V2 ");
+        sb.append("V3 ");
+        sb.append("V4 ");
+
+        System.out.println("stringbuilder:" + sb);
+        sb.deleteCharAt(0);
+        System.out.println("stringbuilder:" + sb.reverse());
+        System.out.println("String:" + sb.toString());
+
+        String word = "holdup";
+        // char[] ch = word.toCharArray();
+        HashSet<Character> result = new HashSet<Character>();
+
+        for (int i = 0; i < word.length(); i++) {
+            result.add(word.charAt(i));
+        }
+        System.out.println(result);
+
+        String str = "";
+        for (int i = 0; i < result.size(); i++) {
+            System.out.print(result.remove(i) + " ");
+        }
+        System.out.println("Ans is:" + str);
+
+
+        // This will working
+        // Iterating over hashSet items
+        Iterator<Character> i = result.iterator();
+
+        // Holds true till there is single element remaining
+        while (i.hasNext())
+
+            // Iterating over elements
+            // using next() method
+            // System.out.println(i.next());
+            str+=i.next();
+
+
+
+            System.out.println("final output:"+str);
+            System.out.println(5>4);
+            System.out.println(5<0);
+            
 
     }
 
