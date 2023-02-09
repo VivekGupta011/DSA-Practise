10dc517a6dd1f72b780aab97f046192a2bbe9934
Vivek Gupta <vivekgupta86988@gmail.com>
2023-02-09 10:18:10 +0530

    probelm

--- OptimizeTechniquesTwoPointerApproach/IntersectionOfTwoArray.java
+++ OptimizeTechniquesTwoPointerApproach/IntersectionOfTwoArray.java
@@ -105,6 +105,7 @@ public class IntersectionOfTwoArray {
         return result;
     }
 
+    
     public static void main(String args[]) {
         int[] nums1 = { 1, 2, 2, 1 };
         int[] nums2 = { 2, 2 };
